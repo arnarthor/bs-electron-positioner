88717f5532c0b1c024f66ddb01962a84155e5714
Arnar Thor Sveinsson <arnar.th92@gmail.com>
2018-09-04 20:59:32 +0000

    More native to reason calculate function

--- src/ElectronPositioner.bs.js
+++ src/ElectronPositioner.bs.js
@@ -2,61 +2,76 @@
 'use strict';
 
 
-function positionsToJs(param) {
+function coordinatesToJs(param) {
   return {
           x: param[/* x */0],
           y: param[/* y */1]
         };
 }
 
-function positionsFromJs(param) {
+function coordinatesFromJs(param) {
   return /* record */[
           /* x */param.x,
           /* y */param.y
         ];
 }
 
-function calculate(t, centerPosition, trayBounds) {
-  return positionsFromJs(t.calculate((function () {
-                      switch (centerPosition) {
-                        case 371331211 : 
-                            return "trayCenter";
-                        case -272647875 : 
-                            return "trayLeft";
-                        case -427234744 : 
-                            return "trayBottomLeft";
-                        case -821073434 : 
-                            return "trayRight";
-                        case -934206853 : 
-                            return "trayBottomRight";
-                        case 371331211 : 
-                            return "trayCenter";
-                        case 912382550 : 
-                            return "trayBottomCenter";
-                        case 907161884 : 
-                            return "topLeft";
-                        case 283497767 : 
-                            return "topRight";
-                        case 591950002 : 
-                            return "bottomLeft";
-                        case 858208465 : 
-                            return "bottomRight";
-                        case -269910486 : 
-                            return "topCenter";
-                        case -958443712 : 
-                            return "bottomCenter";
-                        case 731702620 : 
-                            return "leftCenter";
-                        case -745370959 : 
-                            return "rightCenter";
-                        case 980392437 : 
-                            return "center";
-                        
-                      }
-                    })(), trayBounds));
+function calculate(t, position) {
+  var tmp;
+  if (typeof position === "number") {
+    tmp = t.calculate((function () {
+              switch (position) {
+                case 371331211 : 
+                    return "trayCenter";
+                case -272647875 : 
+                    return "trayLeft";
+                case -427234744 : 
+                    return "trayBottomLeft";
+                case -821073434 : 
+                    return "trayRight";
+                case -934206853 : 
+                    return "trayBottomRight";
+                case 371331211 : 
+                    return "trayCenter";
+                case 912382550 : 
+                    return "trayBottomCenter";
+                case 907161884 : 
+                    return "topLeft";
+                case 283497767 : 
+                    return "topRight";
+                case 591950002 : 
+                    return "bottomLeft";
+                case 858208465 : 
+                    return "bottomRight";
+                case -269910486 : 
+                    return "topCenter";
+                case -958443712 : 
+                    return "bottomCenter";
+                case 731702620 : 
+                    return "leftCenter";
+                case -745370959 : 
+                    return "rightCenter";
+                case 980392437 : 
+                    return "center";
+                
+              }
+            })(), undefined);
+  } else {
+    var variant = position[0];
+    tmp = variant >= -427234744 ? (
+        variant >= 371331211 ? (
+            variant >= 912382550 ? t.calculate("trayBottomCenter", position[1]) : t.calculate("trayCenter", position[1])
+          ) : (
+            variant >= -272647875 ? t.calculate("trayLeft", position[1]) : t.calculate("trayBottomLeft", position[1])
+          )
+      ) : (
+        variant >= -821073434 ? t.calculate("trayRight", position[1]) : t.calculate("trayBottomRight", position[1])
+      );
+  }
+  return coordinatesFromJs(tmp);
 }
 
-exports.positionsToJs = positionsToJs;
-exports.positionsFromJs = positionsFromJs;
+exports.coordinatesToJs = coordinatesToJs;
+exports.coordinatesFromJs = coordinatesFromJs;
 exports.calculate = calculate;
 /* No side effect */
--- src/ElectronPositioner.re
+++ src/ElectronPositioner.re
@@ -4,7 +4,7 @@ type t;
 external make: BsElectron.BrowserWindow.t => t = "electron-positioner";
 
 [@bs.deriving {jsConverter: newType}]
-type positions = {
+type coordinates = {
   x: int,
   y: int,
 };
@@ -13,28 +13,72 @@ type positions = {
 external calculate:
   (
     t,
-    [@bs.string] [
-      | [@bs.as "trayCenter"] `TrayCenter
-      | [@bs.as "trayLeft"] `TrayLeft
-      | [@bs.as "trayBottomLeft"] `TrayBottomLeft
-      | [@bs.as "trayRight"] `TrayRight
-      | [@bs.as "trayBottomRight"] `TrayBottomRight
-      | [@bs.as "trayCenter"] `TrayCenter
-      | [@bs.as "trayBottomCenter"] `TrayBottomCenter
-      | [@bs.as "topLeft"] `TopLeft
-      | [@bs.as "topRight"] `TopRight
-      | [@bs.as "bottomLeft"] `BottomLeft
-      | [@bs.as "bottomRight"] `BottomRight
-      | [@bs.as "topCenter"] `TopCenter
-      | [@bs.as "bottomCenter"] `BottomCenter
-      | [@bs.as "leftCenter"] `LeftCenter
-      | [@bs.as "rightCenter"] `RightCenter
-      | [@bs.as "center"] `Center
-    ],
-    BsElectron.Tray.trayBounds
+    ~position: [@bs.string] [
+                 | [@bs.as "trayCenter"] `TrayCenter
+                 | [@bs.as "trayLeft"] `TrayLeft
+                 | [@bs.as "trayBottomLeft"] `TrayBottomLeft
+                 | [@bs.as "trayRight"] `TrayRight
+                 | [@bs.as "trayBottomRight"] `TrayBottomRight
+                 | [@bs.as "trayCenter"] `TrayCenter
+                 | [@bs.as "trayBottomCenter"] `TrayBottomCenter
+                 | [@bs.as "topLeft"] `TopLeft
+                 | [@bs.as "topRight"] `TopRight
+                 | [@bs.as "bottomLeft"] `BottomLeft
+                 | [@bs.as "bottomRight"] `BottomRight
+                 | [@bs.as "topCenter"] `TopCenter
+                 | [@bs.as "bottomCenter"] `BottomCenter
+                 | [@bs.as "leftCenter"] `LeftCenter
+                 | [@bs.as "rightCenter"] `RightCenter
+                 | [@bs.as "center"] `Center
+               ],
+    ~trayBounds: BsElectron.Tray.trayBounds=?,
+    unit
   ) =>
-  abs_positions =
+  abs_coordinates =
   "";
 
-let calculate = (t, centerPosition, trayBounds) =>
-  calculate(t, centerPosition, trayBounds)->positionsFromJs;
+type position = [
+  | `TrayLeft(BsElectron.Tray.trayBounds)
+  | `TrayBottomLeft(BsElectron.Tray.trayBounds)
+  | `TrayRight(BsElectron.Tray.trayBounds)
+  | `TrayBottomRight(BsElectron.Tray.trayBounds)
+  | `TrayCenter(BsElectron.Tray.trayBounds)
+  | `TrayBottomCenter(BsElectron.Tray.trayBounds)
+  | `TopLeft
+  | `TopRight
+  | `BottomLeft
+  | `BottomRight
+  | `TopCenter
+  | `BottomCenter
+  | `LeftCenter
+  | `RightCenter
+  | `Center
+];
+
+let calculate = (t, position: position) =>
+  (
+    switch (position) {
+    | `TrayLeft(trayBounds) =>
+      calculate(t, ~position=`TrayLeft, ~trayBounds, ())
+    | `TrayBottomLeft(trayBounds) =>
+      calculate(t, ~position=`TrayBottomLeft, ~trayBounds, ())
+    | `TrayRight(trayBounds) =>
+      calculate(t, ~position=`TrayRight, ~trayBounds, ())
+    | `TrayBottomRight(trayBounds) =>
+      calculate(t, ~position=`TrayBottomRight, ~trayBounds, ())
+    | `TrayCenter(trayBounds) =>
+      calculate(t, ~position=`TrayCenter, ~trayBounds, ())
+    | `TrayBottomCenter(trayBounds) =>
+      calculate(t, ~position=`TrayBottomCenter, ~trayBounds, ())
+    | `TopLeft as position
+    | `TopRight as position
+    | `BottomLeft as position
+    | `BottomRight as position
+    | `TopCenter as position
+    | `BottomCenter as position
+    | `LeftCenter as position
+    | `RightCenter as position
+    | `Center as position => calculate(t, ~position, ())
+    }
+  )
+  ->coordinatesFromJs;
